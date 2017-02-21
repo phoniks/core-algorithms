4dbc0e21fee7ecbc822172d3dd945229eb0b6fc6
John Hopkins <johnthopkins@gmail.com>
2017-02-21 11:44:22 -0800

    completed make change as a warmup

--- src/makeChange.js
+++ src/makeChange.js
@@ -1,3 +1,31 @@
 export default function makeChange({price, amountGiven}) {
-  // your code here
+  if(amountGiven < price){
+    console.log('This is insuffecient currency to purchase that item!')
+  }
+
+  let change = {quarters: 0, dimes: 0, nickels: 0, pennies: 0}
+  let changeDue = amountGiven - price
+  if(changeDue === 0){
+    return change
+  }
+  while(changeDue >= 25){
+    change.quarters += 1
+    changeDue -= 25
+  }
+
+  while(changeDue >= 10){
+    change.dimes += 1
+    changeDue -= 10
+  }
+
+  while(changeDue >= 5){
+    change.nickels += 1
+    changeDue -= 5
+  }
+
+  while(changeDue > 0){
+    change.pennies += 1
+    changeDue -= 1
+  }
+  return change
 }
